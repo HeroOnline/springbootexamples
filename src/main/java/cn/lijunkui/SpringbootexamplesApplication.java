95e3e790dcb4607fd7d9ebb2b4b9e9e70543ccea
zhuoqianmingyue <ljk126wy@126.com>
2019-02-24 21:40:41 +0800

    添加自动配置相关示例程序

--- src/main/java/cn/lijunkui/SpringbootexamplesApplication.java
+++ src/main/java/cn/lijunkui/SpringbootexamplesApplication.java
@@ -3,7 +3,10 @@ package cn.lijunkui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import cn.lijunkui.enable.EnableHttpClient;
+
 @SpringBootApplication
+@EnableHttpClient()
 public class SpringbootexamplesApplication {
 
 	public static void main(String[] args) {
